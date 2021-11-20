1312b9d4dd204dac3cbe813a71e776d5071a86c5
Rishub Nagpal <rishub.nagpal@lamarr.at>
2021-11-21 00:08:52 +0100

    update

--- fastvcd.py
+++ fastvcd.py
@@ -1,3 +1,4 @@
+import re
 from bisect import bisect_right
 class Signal:
     def __init__(self) -> None:
@@ -12,40 +13,53 @@ class Signal:
         x = bisect_right(self.indices, i)
         return self.data[x-1]
 
-def scope_str(heir):
-    return '.'.join(heir)
 
-def parse_vcd(f):
-    signals = {"id": {}, "name":{}}
-    heir = []
-    t = 0
-    with open(f, 'r') as vcd:
-        for line in vcd:
-            if "$var" in line:
-                l = line.split(" ")
-                s = Signal()
-                sc = scope_str(heir)
-                if l[3] in signals["id"]:
-                    signals["name"][f'{sc}.{l[4]}'] = signals["id"][l[3]]
+class FastVCD:
+    def __init__(self, vcd):
+        self.signals = self.parse_vcd(vcd)
+
+    def parse_vcd(self, f):
+        signals = {"id": {}, "name":{}}
+        hier = []
+        t = 0
+        with open(f, 'r') as vcd:
+            for line in vcd:
+                if "$var" in line:
+                    l = line.split(" ")
+                    s = Signal()
+                    sc = '.'.join(hier)
+                    if len(l) == 7:
+                        name = f'{sc}.{l[4]}{l[5]}'
+                    else:
+                        name = f'{sc}.{l[4]}'
+                    if l[3] in signals["id"]:
+                        signals["name"][name] = signals["id"][l[3]]
+                    else:
+                        signals["id"][l[3]] = s
+                        signals["name"][name] = s
+                elif "$scope" in line:
+                    l = line.split(" ")
+                    hier.append(l[2])
+                elif "$upscope" in line:
+                    hier.pop()
+                elif '$date' in line or '$version' in line:
+                    while '$end' not in next(vcd):
+                        pass
+                elif '$timescale' in line:
+                    l = next(vcd)
+                    self.timescale = l.strip()
+                    while '$end' not in next(vcd):
+                        pass
+                elif line[0] == '#':
+                    t = int(line[1:].strip())
+                elif line.isspace() or line[0] == '$':
+                    pass
                 else:
-                    signals["id"][l[3]] = s
-                    signals["name"][f'{sc}.{l[4]}'] = s
-            elif "$scope" in line:
-                l = line.split(" ")
-                heir.append(l[2])
-            elif "$upscope" in line:
-                heir.pop()
-            elif line[0] == '#':
-                t = int(line[1:].strip())
-            elif line[0] == '$':
-                pass
-            else:
-                l = list(line)
-                id = l[1]
-                x = int(l[0])
-                signals['id'][id].add(t, x)
-    return signals
+                    l = line.strip()
+                    id = l[1:]
+                    x = l[0] if l[0] == 'x' else int(l[0])
+                    signals['id'][id].add(t, x)
+        return signals
 
 
-signals = parse_vcd('test.vcd')
-print(signals)
+signals = FastVCD('trace_0.vcd')
